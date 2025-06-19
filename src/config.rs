f432d93eae65a8dbffd4672a3f36b0c178a63e53
als <57554507+2220258345@users.noreply.github.com>
2025-06-19 19:06:00 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["desk.huaqiweilai.xin"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["desk.huaqiweilai.top"];
 pub const RS_PUB_KEY: &str = "SvxFcUxMiL2dcgOXTbfpqc0CJbg2s2V10L2JuYVwSTc=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
